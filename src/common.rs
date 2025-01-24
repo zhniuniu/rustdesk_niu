07a7f593eb2166c253bba17bdb65295444ba1ccc
rustdesk_niu <859909009@qq.com>
2025-01-24 12:03:23 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -910,7 +910,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://home.zhniuniu.cn".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
